1d494122eee7ca18804cad019cdcfec4becea366
Ankit Mishra <ankitm8421@gmail.com>
2023-03-03 21:28:56 +0530

    commited successfully

--- Easy/Best Time to buy and sell stocks/solution.py
+++ Easy/Best Time to buy and sell stocks/solution.py
@@ -6,8 +6,7 @@ https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 """
 Intitution:
 The idea is to obtain the maximum profit we buy on the day where the stock is minimum and sell on the day where the profit from minimum is maximum.
-
-  
+ 
 Algorithm:
   We assume the minimum stock price to be on day1 
   then we iterate over the array if the element is lower than the current minimum we update minimum 
