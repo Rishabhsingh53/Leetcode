189c14a213f3ea1f11a89b291b01db63fc0b32c2
Rishabh Singh <rishabhsingh50689@gmail.com>
2023-03-10 19:24:24 +0530

    initial commit

--- Medium/Find the Index of the First Occurrence in a String/solution.java
+++ Medium/Find the Index of the First Occurrence in a String/solution.java
@@ -1,5 +1,5 @@
 /* 
-287. Find the Duplicate Number https://leetcode.com/problems/find-the-duplicate-number/
+287. Find the Index of the First Occurrence in a String.
 
 Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
 There is only one repeated number in nums, return this repeated number.
